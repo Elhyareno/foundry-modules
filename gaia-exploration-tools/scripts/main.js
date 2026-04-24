8328e267114a412df768b48c659c534a858cb697
Elhyareno <mathieu.groult@gmail.com>
2026-04-24 19:43:26 +0200

    MAJ

--- gaia-exploration-tools/scripts/main.js
+++ gaia-exploration-tools/scripts/main.js
@@ -4,10 +4,6 @@ import { GaiaExplorationDialog } from "./ui/GaiaExplorationDialog.js";
 Hooks.once("ready", () => {
   ui.notifications.info("Gaïa Exploration Tools chargé.");
 
-  ChatMessage.create({
-        content: "Le module gaiaExploration est bien chargé"
-  });
-  
   const generator = new GaiaGenerator();
 
   game.gaiaExploration = {
@@ -27,24 +23,6 @@ Hooks.once("ready", () => {
       });
 
       return event;
-    },
-
-    envoyerEvenementTest(){
-        const evenement = {
-            titre: "Mue de l'essaim",
-            biome: "jungle",
-            danger: 3,
-            description: "Une membrane chitineuse géante pend entre deux arbres. Elle est encore tiède."
-        };
-        const contenu = `
-        <h2>${evenement.titre}</h2>
-        <p><strong>Biome :</strong> ${evenement.biome}</p>
-        <p>${evenement.description}</p>
-        <p><strong>Danger :</strong> ${evenement.danger}</p>
-        `;
-        ChatMessage.create({
-            content: contenu
-        });
     }
   };
 });
