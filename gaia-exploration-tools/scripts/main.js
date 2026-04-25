6ae060f4e2fe4e1cfa2370d5ad7798aa6a6d81b1
Elhyareno <mathieu.groult@gmail.com>
2026-04-25 15:55:40 +0200

    MAJ: Update GaiaExplorationService and GaiaGenerator to include rollType in formatting functions

--- gaia-exploration-tools/scripts/main.js
+++ gaia-exploration-tools/scripts/main.js
@@ -16,3 +16,14 @@ Hooks.once("ready", () => {
 
   ui.notifications.info("Gaïa Exploration Tools chargé.");
 });
+
+Hooks.on("renderChatMessage", (message, html, data) => {
+  html.find(".gaia-reroll").click(ev => {
+    const button = ev.currentTarget;
+
+    const rollType = button.dataset.rollType;
+    const biome = button.dataset.biome;
+
+    game.gaiaExploration.rollByType(rollType, biome);
+  });
+});
