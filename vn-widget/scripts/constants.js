d9245c4e3f274ef226fd52bd13a83e54aef64de8
Elhyareno <mathieu.groult@gmail.com>
2026-04-28 22:58:49 +0200

    fix

--- vn-widget/scripts/constants.js
+++ vn-widget/scripts/constants.js
@@ -1,2 +1,15 @@
 export const MODULE_ID = "vn-widget";
 export const SOCKET_NAME = `module.${MODULE_ID}`;
+
+// Flags utilisés pour stocker le Vitality Network sur l’acteur
+export const FLAG_SCOPE = MODULE_ID;
+export const FLAG_KEY = "vitalityNetwork";
+
+// Alias de compatibilité, au cas où d’anciens fichiers utilisent ces noms
+export const VN_FLAG_SCOPE = FLAG_SCOPE;
+export const VN_FLAG_KEY = FLAG_KEY;
+
+// Identifiants lisibles pour les ressources / affichages
+export const RESOURCE_SLUG = "vitality-network";
+export const RESOURCE_KEY = "vitalityNetwork";
+export const RESOURCE_LABEL = "Vitality Network";
